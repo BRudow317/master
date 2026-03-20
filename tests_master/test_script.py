7c6c7bf010be0c70f319364b5618e815a3de2085
BRudow317 <blainerudow@gmail.com>
2026-03-19 21:46:03 -0400

    consolidated to one file. Cut many features.

--- tests_master/test_script.py
+++
@@ -1,9 +0,0 @@
-import cryptography
-
-"""
-python master.py --venv ./tests_master/.venv --exec python ./tests_master/test_script.py
-"""
-
-print ("-- Testing Start --")
-print(cryptography.__version__ == "46.0.5")
-print ("-- Testing End --")
